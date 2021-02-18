29272ff0ff9e6d0a8ff80e8855b76185d7b77b97
Валерий Тимошенко <timoshenko.valerii@outlook.com>
2021-02-18 09:44:31 +0300

    add ContainsInt

--- utils.go
+++ utils.go
@@ -1,7 +1,17 @@
 package utils
 
-//Contains ... return true when a contains
+//Contains ... return true when slice a contains string x
 func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+//ContainsInt ... return true when slice a contains int x
+func ContainsInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
 			return true
