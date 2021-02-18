63b479228a873fc65bd3f561ceae2034bd71f5f9
Валерий Тимошенко <timoshenko.valerii@outlook.com>
2021-02-18 21:11:39 +0300

    Change func name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Timocha/utils
+module github.com/Timocha/utils/v2
 
 go 1.15
--- utils.go
+++ utils.go
@@ -1,7 +1,7 @@
 package utils
 
-//Contains ... return true when slice a contains string x
-func Contains(a []string, x string) bool {
+//InSlice ... return true when slice a contains string x
+func InSlice(a []string, x string) bool {
 	for _, n := range a {
 		if x == n {
 			return true
